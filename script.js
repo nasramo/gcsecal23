e0978c0c1da8612ef4d5530947374ded7ece2227
nasramo <103969243+nasramo@users.noreply.github.com>
2023-04-10 13:31:27 +0100

    create + copy template

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover({
+      container: 'body'
+  });
+}); 
+$('.popover-dismiss').popover({
+  trigger: 'focus'
+});
+$(document).ready(function(){
+    $("#myModal").modal('show');
+});
+//This was the most annoying thing to mess with in my life. I ended up limiting my use of JS to what was necessary to allow popovers to function, and even those were actually copied from the BS docs.
